a136d805fe044e3449af4233a41c8eb8f1959cb5
chronologisch <bastianwoidt@gmail.com>
2023-02-14 13:47:12 +0100

    Observability working

--- CarService/src/main/java/com/daprdemo/CarService/Controller/CarController.java
+++ CarService/src/main/java/com/daprdemo/CarService/Controller/CarController.java
@@ -1,6 +1,8 @@
 package com.daprdemo.CarService.Controller;
 
 import com.daprdemo.CarService.Services.EnabledService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+
 @RestController
 public class CarController {
+
+    Logger logger = LoggerFactory.getLogger(CarController.class);
 
     @Autowired
     EnabledService enabledService;
@@ -19,15 +24,16 @@ public class CarController {
 
     @GetMapping("/car/{id}")
     public ResponseEntity<String> getCar(@PathVariable String id){
+        logger.info("This is a log for id "+ id);
         System.out.println("enabled is "+ enabledService.isEnabled()+ " and request id is " + id);
         if(!enabledService.isEnabled()) return ResponseEntity.badRequest().body("API is not enabled");
         return ResponseEntity.of(Optional.of("You have successfully requested a car with the id: " + id));
     }
 
-//    @GetMapping("/health")
-//    public ResponseEntity<String> getHealth(){
-//        return getCar("1");
-//    }
+    @GetMapping("/health")
+    public ResponseEntity<String> getHealth(){
+        return getCar("1");
+    }
 
     @PostMapping("/enabled/{enabled}")
     public String setEnabled(@PathVariable boolean enabled){
--- Worker/startWorker.sh
+++ Worker/startWorker.sh
@@ -1,3 +1,5 @@
 dapr stop worker
 
 mvn clean package
+
+dapr run --app-id worker --app-port 8080 --config ../config.yaml --components-path ../Components/ -- java -jar ./target/Worker-0.0.1-SNAPSHOT.jar --server.port=8080
